59744955fd1e3a223b166095f71bad837af05776
dy <dy@alfa.im>
2023-04-08 01:23:51 +0300

    t-22 revert intradaily-exercises table layout 1.0

--- resources/views/main/view.blade.php
+++ resources/views/main/view.blade.php
@@ -22,73 +22,32 @@
 
         <div id="intradaily-exercises" class="mt-4">
 
-{{--            <div class="row mb-2">--}}
-{{--                <div class="text-start sort-field">#</div>--}}
-{{--                <div class="col-3 text-start">упраж&shyнение</div>--}}
-{{--                <div class="col-2 text-start">повто&shyрений</div>--}}
-{{--                <div class="col text-start"><span class="">комментарий</span></div>--}}
-{{--                <div class="col-1"></div>--}}
-{{--            </div>--}}
-
             <div class="row mb-2">
-                <div class="text-start sort-field" style="width: 4%">#</div>
-                <div class="text-start" style="width: 28%">упраж&shyнение</div>
-                <div class="text-start" style="width: 12%">повто&shyрений</div>
-                <div class="text-start" style="width: 40%"><span class="">комментарий</span></div>
-                <div class="" style="width: 16%"></div>
+                <div class="text-start sort-field">#</div>
+                <div class="col-3 text-start">упраж&shyнение</div>
+                <div class="col-2 text-start">повто&shyрений</div>
+                <div class="col text-start"><span class="">комментарий</span></div>
+                <div class="col-1"></div>
             </div>
 
             <div id="intradaily-exercises-body">
 
-{{--                @foreach($user_physical_exercises as $user_physical_exercise)--}}
-{{--                    <div draggable="true" class="row block-body">--}}
-{{--                        <div class="text-start sort-field physical-exercise-{{$user_physical_exercise['id']}}">--}}
-{{--                            {{$user_physical_exercise['intraday_key']}}--}}
-{{--                        </div>--}}
-{{--                        <div class="col-3 text-start physical-exercise-{{$user_physical_exercise['id']}}">--}}
-{{--                            <span>{{$user_physical_exercise['physical_exercises']['name']}}</span>--}}
-{{--                        </div>--}}
-{{--                        <div class="col-2">--}}
-{{--                            <div class="input-parent border-bottom">--}}
-{{--                                <input type="text" value="{{ $user_physical_exercise['count'] }}"--}}
-{{--                                       name="pe-count-{{$user_physical_exercise['id']}}"--}}
-{{--                                       class="item-count" autocomplete="none">--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        <div class="col">--}}
-{{--                            <div class="input-parent border-bottom">--}}
-{{--                                <input type="text" value="{{ $user_physical_exercise['comment'] }}"--}}
-{{--                                       name="pe-comment-{{$user_physical_exercise['id']}}"--}}
-{{--                                       class="item-comment"--}}
-{{--                                       autocomplete="none">--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        <div class="col-1 delete-control">--}}
-{{--                            <div class="w-50 h5" style="margin-left: -5%">--}}
-{{--                                <i id="i-element-{{$user_physical_exercise['id']}}" class="bi bi-x"></i>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                @endforeach--}}
-
-
                 @foreach($user_physical_exercises as $user_physical_exercise)
                     <div draggable="true" class="row block-body">
-
-                        <div class="text-start sort-field physical-exercise-{{$user_physical_exercise['id']}}" style="width: 4%">
+                        <div class="text-start sort-field physical-exercise-{{$user_physical_exercise['id']}}">
                             {{$user_physical_exercise['intraday_key']}}
                         </div>
-                        <div class=" text-start physical-exercise-{{$user_physical_exercise['id']}}" style="width: 28%">
+                        <div class="col-3 text-start physical-exercise-{{$user_physical_exercise['id']}}">
                             <span>{{$user_physical_exercise['physical_exercises']['name']}}</span>
                         </div>
-                        <div class="" style="width: 18%">
+                        <div class="col-2">
                             <div class="input-parent border-bottom">
                                 <input type="text" value="{{ $user_physical_exercise['count'] }}"
                                        name="pe-count-{{$user_physical_exercise['id']}}"
                                        class="item-count" autocomplete="none">
                             </div>
                         </div>
-                        <div class="" style="width: 40%">
+                        <div class="col">
                             <div class="input-parent border-bottom">
                                 <input type="text" value="{{ $user_physical_exercise['comment'] }}"
                                        name="pe-comment-{{$user_physical_exercise['id']}}"
@@ -96,8 +55,8 @@
                                        autocomplete="none">
                             </div>
                         </div>
-                        <div class="delete-control" style="width: 6%">
-                            <div class="w-50 h5">
+                        <div class="col-1 delete-control">
+                            <div class="w-50 h5" style="margin-left: -5%">
                                 <i id="i-element-{{$user_physical_exercise['id']}}" class="bi bi-x"></i>
                             </div>
                         </div>
