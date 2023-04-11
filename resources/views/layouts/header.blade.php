3850608099ba56702ee7306f1524ce63d54e652f
dy <dy@alfa.im>
2023-04-12 01:40:05 +0300

    t-24 fix mobile pagination 1.1

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,58 +1,60 @@
-<header class="@if(request('device_type') != 'computer') header-mobile @endif">
+<header>
 
     @if(in_array(request('device_type'), ['phone', 'tablet']))
-        <div class="container">
-            <a href="{{route('main.index')}}" class="logo">EJ</a>
+        <div class="header-mobile">
+            <div class="container">
+                <a href="{{route('main.index')}}" class="logo">EJ</a>
 
-            <input class="side-menu" type="checkbox" id="side-menu"/>
-            <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
+                <input class="side-menu" type="checkbox" id="side-menu"/>
+                <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
 
-            <div class="row mobile-menu-block">
+                <div class="row mobile-menu-block">
 
-                <div class="col">
-                    <nav class="nav">
-                        <ul class="menu">
-                            <li>
-                                <a class="nav-link" href="{{route('main.index')}}">
-                                    главная
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-
-                <div class="col">
-                    <nav class="nav">
-                        <ul class="menu">
-                            <li class="">
-                                <div class="text-nowrap overflow-hidden" style="width: 30vw;">
-                                    <a class="nav-link text-nowrap" href="{{route('profile.index')}}">
-                                        @auth()
-                                            {{ \Illuminate\Support\Facades\Auth::user()->name}}
-                                        @endauth
+                    <div class="col">
+                        <nav class="nav">
+                            <ul class="menu">
+                                <li>
+                                    <a class="nav-link" href="{{route('main.index')}}">
+                                        главная
                                     </a>
-                                </div>
-                            </li>
-                            <li>
-                                <a class="nav-link" href="{{route('settings.index')}}">
-                                    настройки
-                                </a>
-                            </li>
-                            <li>
-                                <form method="POST" class="" action="{{ route('logout') }}">
-                                    @csrf
-                                    <a class="cursor-pointer"
-                                       onclick="event.preventDefault(); this.closest('form').submit();">
-                                        выход
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+
+                    <div class="col">
+                        <nav class="nav">
+                            <ul class="menu">
+                                <li class="">
+                                    <div class="text-nowrap overflow-hidden" style="width: 30vw;">
+                                        <a class="nav-link text-nowrap" href="{{route('profile.index')}}">
+                                            @auth()
+                                                {{ \Illuminate\Support\Facades\Auth::user()->name}}
+                                            @endauth
+                                        </a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a class="nav-link" href="{{route('settings.index')}}">
+                                        настройки
                                     </a>
-                                </form>
-                            </li>
-                        </ul>
-                    </nav>
+                                </li>
+                                <li>
+                                    <form method="POST" class="" action="{{ route('logout') }}">
+                                        @csrf
+                                        <a class="cursor-pointer"
+                                           onclick="event.preventDefault(); this.closest('form').submit();">
+                                            выход
+                                        </a>
+                                    </form>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+
                 </div>
 
             </div>
-
         </div>
     @endif
 
