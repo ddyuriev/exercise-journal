441164e4c64f6a7d1ddff3d8c8b1b83e9d8a2184
dy <dy@alfa.im>
2023-04-19 03:01:17 +0300

    t-24 register user 0.1

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,29 +6,30 @@
             class="container p-4 mb-4 d-flex justify-content-center @if(request('device_type') != 'computer') mt-4 @endif">
             <div class="col-4">
                 <div class="block-login justify-content-center position-relative">
-                    <form method="POST" action="{{ route('login') }}">
+                    <form method="POST" action="{{ route('register') }}">
                         @csrf
                         <div class="text-center">
-                            <h4 class="mb-4">Вход</h4>
+                            <h4 class="mb-4">Регистрация</h4>
+                        </div>
+                        <div class="mb-3">
+                            <input name="name" class="form-control" placeholder="Логин">
                         </div>
                         <div class="mb-3">
                             <input name="email" type="email" class="form-control" aria-describedby="emailHelp"
-                                   placeholder="Логин">
+                                   placeholder="Email">
                         </div>
                         <div class="mb-3">
                             <input name="password" type="password" class="form-control" placeholder="Пароль">
                         </div>
+                        <div class="mb-3">
+                            <input name="password_confirmation" type="password" class="form-control"
+                                   placeholder="Повторите пароль">
+                        </div>
 
                         <div class="mb-3 text-left">
-                            <button type="submit" class="btn btn-primary">Войти</button>
+                            <button type="submit" class="btn btn-primary">Зарегистрироваться</button>
                         </div>
                     </form>
-
-{{--                    <div class="text-left position-absolute bottom-0 end-0">--}}
-{{--                        <button type="submit" class="btn btn-light" onclick="window.location.href='register'">--}}
-{{--                            Регистрация--}}
-{{--                        </button>--}}
-{{--                    </div>--}}
                 </div>
             </div>
         </div>
